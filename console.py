343be8be384f57c07b81d4b047ad84f3d22a608f
obigerard18 <obigerard18@gmail.com>
2023-02-11 03:10:19 -0800

    console

--- console.py
+++ console.py
@@ -240,3 +240,29 @@ class HBNBCommand(cmd.Cmd):
         """retrieve all instances of a class and
         retrieve the number of instances
         """
+        my_list = line.split('.')
+        if len(my_list) >= 2:
+            if my_list[1] == "all()":
+                self.do_all(my_list[0])
+            elif my_list[1] == "count()":
+                self.count(my_list[0])
+            elif my_list[1][:4] == "show":
+                self.do_show(self.strip_clean(my_list))
+            elif my_list[1][:7] == "destroy":
+                self.do_destroy(self.strip_clean(my_list))
+            elif my_list[1][:6] == "update":
+                args = self.strip_clean(my_list)
+                if isinstance(args, list):
+                    obj = storage.all()
+                    key = args[0] + ' ' + args[1]
+                    for k, v in args[2].items():
+                        self.do_update(key + ' "{}" "{}"'.format(k, v))
+                    else:
+                        self.do_update(args)
+            else:
+                cmd.Cmd.default(self, line)
+
+
+
+if __name__ == '__main__':
+    HBNBCommand().cmdloop()
